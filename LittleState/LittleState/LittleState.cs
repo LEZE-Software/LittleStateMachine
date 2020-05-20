c8e633ae872cfc5a4dfd6e6bbdf044a63b45d501
LeoHome <30297868+MTB1Leo@users.noreply.github.com>
2020-05-20 13:50:55 +0200

    added dll ile

---
+++ LittleState/LittleState/LittleState.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleState
+{
+    public abstract class LittleEvent
+    {
+        private EventState _current = 0;
+        public EventState current
+
+        {
+            get { return _current; }
+            set { _current = value; }
+        }
+    
+        private bool _isRoot;
+        /// <summary>
+        /// The root event shall be the only event which can stop the program if it has finished.
+        /// </summary>
+        /// 
+        public abstract EventInformation info();
+
+        public void ChangeTo(EventState newState)
+        {
+            _current = newState;
+        }
+
+        public abstract void Fail(StateArgs arg);
+        public abstract ReturnObject Init();
+        public abstract ReturnObject Done();
+        public abstract ReturnObject Idle();
+        public abstract ReturnObject Finish();
+    }
+
+    public abstract class LittleProgram
+    {
+        public mainState state;
+        public abstract void Done();
+        public abstract void Fail();
+        public abstract ReturnObject Paused();
+    }
+}
---
+++ LittleState/LittleState/ValueFile.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleState
+{
+    public struct EventInformation
+    {
+        public bool isRoot;
+        public string eventName;
+    }
+
+    public struct ReturnObject
+    {
+        public FunctionResult returnState;
+        public StateArgs returnArguments;
+        public bool shallContinueProgram;
+    }
+
+    public class State
+    {
+        public EventState current = 0;
+
+        public void Next()
+        {
+            current++;
+        }
+
+        public void Previous()
+        {
+            current--;
+        }
+
+        public virtual void Init()
+        {
+
+        }
+    }
+
+    public struct StateArgs
+    {
+        public Exception failException;
+        public dynamic payLoad;
+    }
+
+    public enum mainState
+    {
+        Running,
+        Paused,
+        Done,
+        Fail,
+        LAST_INDEX
+    }
+
+    public enum EventState
+    {
+        Init,
+        Idle,
+        Finish,
+        Done,
+        Fail,
+        LAST_INDEX
+    }
+
+    /// <summary>
+    /// Result options for functions of type ReturnObject.
+    /// </summary>
+    public enum FunctionResult
+    {
+        Success,
+        Fail,
+        LAST_INDEX
+    }
+}
